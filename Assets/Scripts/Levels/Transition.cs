1e76e2f35f818308e3f7b6bbf9b752ce1179d06d
MHermosillo <143014585+MHermosillo7@users.noreply.github.com>
2024-05-23 22:14:51 -0600

    Documented Transition script
    All grapple related scripts have been fully documented as of the moment

--- Assets/Scripts/Levels/Transition.cs
+++ Assets/Scripts/Levels/Transition.cs
@@ -5,17 +5,21 @@ namespace Heaven
     [RequireComponent(typeof(Collider2D))]
     public class Transition : MonoBehaviour
     {
-        Collider2D collider;
-        Animator animator;
+        Collider2D collider;    //Collider reference
+        Animator animator;      //Animator reference
         // Start is called before the first frame update
         void Awake()
         {
+            //Get Collider component and make it a trigger
             collider = GetComponent<Collider2D>();
             collider.isTrigger = true;
 
+            //Find animator of object called CircleWipe
             animator = GameObject.Find("CircleWipe").GetComponent<Animator>();
         }
 
+        //When object is triggered by Player
+        //Set animator's 'Start' trigger
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.gameObject.CompareTag("Player"))

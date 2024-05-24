75385c631d4e33ed8afa8e433ff241600cbe0e1e
MHermosillo <143014585+MHermosillo7@users.noreply.github.com>
2024-05-23 21:47:23 -0600

    Documented GrapplingGun script

--- Assets/Scripts/Grapple/GrapplingGun.cs
+++ Assets/Scripts/Grapple/GrapplingGun.cs
@@ -49,7 +49,7 @@ namespace Heaven
             Transform,
             Physics
         }
-
+        
         [Header("Launching:")]
         //Whether move player to grappled object
         [SerializeField] public bool launchToPoint = true;
@@ -115,49 +115,74 @@ namespace Heaven
                 //If GrapplingRope is enabled
                 if (grappleRope.enabled)
                 {
+                    //Fix gun pivot's rotation angle toward grappled point
                     RotateGun(grapplePoint, false);
+
+                    //Deactivate aim game object
                     aim.SetActive(false);
                 }
+                //Else GrapplingRope is diabled
                 else
                 {
+                    //Rotate gun pivot toward mouse' position
                     Vector2 mousePos = camera.ScreenToWorldPoint(
                         Input.mousePosition);
                     RotateGun(mousePos, true);
                 }
 
+                //If player should be launched to grappled point
+                //And player is grappling
                 if (launchToPoint && grappleRope.isGrappling)
                 {
+                    //If type of launch is of Transform system
                     if (launchType == LaunchType.Transform)
                     {
+                        //Create new distance vector 2 that equals
+                        //fire point position minus player's local position
                         Vector2 firePointDistnace =
                             firePoint.position - player.localPosition;
 
+                        //Create new vector 2 to store target position
+                        //equals grappled point minus previous distance vector
                         Vector2 targetPos = grapplePoint - firePointDistnace;
+
+                        //Move Player's position through interpolations by
+                        //moving from current position to target position
+                        //at a frame independant rate times launch speed
                         player.position =
                             Vector2.Lerp(player.position, targetPos,
                             Time.deltaTime * launchSpeed);
                     }
                 }
             }
+            //Else if left or right click are no longer pressed
+            //or all grapple related method should be stopped
             else if (Input.GetMouseButtonUp(0) || Input.GetMouseButtonUp(1) 
                 || resetGrapple)
             {
-                resetGrapple = false;
-                aim.SetActive(true);
-                grappleRope.enabled = false;
-                joint2D.enabled = false;
-                rb.gravityScale = 1;
+                resetGrapple = false;        //Not stop grapple methods anymore
+                aim.SetActive(true);         //Activate aim
+                grappleRope.enabled = false; //Disable GrapplingRope script
+                joint2D.enabled = false;     //Disable SpringJoint2D component
+                rb.gravityScale = 1;         //Reset player's gravity to 1
 
+                //if there is a ControlObject script reference
                 if (controller)
                 {
+                    //Call for reference's Deactivate coroutine
                     StartCoroutine(controller.Deactivate());
+                    //Make controller reference null
                     controller = null;
                 }
+                //If there is no ControlObject script reference
+                //It is null
                 else controller = null;
 
             }
+            //If no button has been clicked
             else
             {
+                //Rotate gun pivot toward mouse' position
                 Vector2 mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
                 RotateGun(mousePos, true);
             }
@@ -165,40 +190,64 @@ namespace Heaven
 
         void RotateGun(Vector3 lookPoint, bool allowRotationOverTime)
         {
+            //Create Vector of distance between point looked and gun pivot position
             Vector3 distanceVector = lookPoint - gunPivot.position;
 
+            //Create float to store angle of distance vector 
+            //after converted to radians and back to degrees
             float angle = Mathf.Atan2(distanceVector.y, distanceVector.x) * Mathf.Rad2Deg;
 
+            //If gun pivot should rotate over time
             if (rotateOverTime && allowRotationOverTime)
             {
+                //Rotate gun pivot by Quaternion interpolation 
+                //between its rotation and that of angle degrees in the forward axis
+                //at a frame independant rate times speed of rotation
                 gunPivot.rotation = Quaternion.Lerp(gunPivot.rotation,
                     Quaternion.AngleAxis(angle, Vector3.forward),
                     Time.deltaTime * rotationSpeed);
             }
+            //If gun pivot should not be rotated
             else
             {
+                //Rotate gun pivot by angle degrees in the forward axis
                 gunPivot.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
             }
         }
 
         void SetGrapplePoint()
         {
+            //Distance between mouse and gun pivot
             Vector2 distanceVector = 
                 camera.ScreenToWorldPoint(Input.mousePosition) - gunPivot.position;
 
+            //FIre a raycast from fire point to the direction of distance vector
             if (Physics2D.Raycast(firePoint.position, distanceVector.normalized))
             {
+                //Instantiate a RaycastHit with previous values in if statement
                 RaycastHit2D _hit = Physics2D.Raycast(firePoint.position, distanceVector.normalized);
+
+                //If object hit's layer number equals the grappable layer number
+                //or player can grapple to all layer objects
                 if (_hit.transform.gameObject.layer == grappableLayerNumber || grappleToAll)
                 {
+                    //If distance between object hit and fire point is less than or equal to max distance
+                    //or there is no maximum distance
                     if (Vector2.Distance(_hit.point, firePoint.position) <= maxDistance || !hasMaxDistance)
                     {
+                        //Get reference of hit object's ControlObject script
                         controller = _hit.transform.gameObject.
                         GetComponent<ControlObject>();
 
+                        //grappled point equals hit point
                         grapplePoint = _hit.point;
+
+                        //Grappled Distance Vector equals grappled point minus
+                        //gun pivot's position
                         grappleDistanceVector =
                             grapplePoint - (Vector2)gunPivot.position;
+
+                        //Enable GrapplingRope script
                         grappleRope.enabled = true;
                     }
                 }
@@ -207,42 +256,63 @@ namespace Heaven
 
         public void Grapple()
         {
+            //Disable SpringJoint2D's from auto configuring the distance
             joint2D.autoConfigureDistance = false;
 
+            //If player should not be moved to grappled point
+            //or distance between the two should not be auto configured
             if (!launchToPoint && !autoConfigureDistance)
             {
+                //SpringJoing2D's distance equals target distance
+                //And its frequency equals target frequency
                 joint2D.distance = targetDistance;
                 joint2D.frequency = targetFrequency;
             }
 
+            //If player should not be moved to grappled point
             if (!launchToPoint)
             {
+                //If distance between the two should be controlled
                 if (autoConfigureDistance)
                 {
+                    //Make SpringJoint2D auto configure the distance
+                    //And change its frequency to 0
                     joint2D.autoConfigureDistance = true;
                     joint2D.frequency = 0;
                 }
 
+                //SpringJoint2D's connected anchor equals grappled point
+                //And enable SpringJoint2D component
                 joint2D.connectedAnchor = grapplePoint;
                 joint2D.enabled = true;
             }
+            //Else player should not be laucnhed to grappled point
             else
-            {
+            {    
+                //Switch launch system in place
                 switch (launchType)
                 {
+                    //In case system is type Physics
                     case LaunchType.Physics:
+
+                        //SpringJoint2D's anchor equals grappled point
                         joint2D.connectedAnchor = grapplePoint;
 
+                        //Distance Vector equals fire point position minus player's
                         Vector2 distanceVector = firePoint.position - player.position;
 
+                        //SpringJoint2D's distance equals magnitude of previous vector
+                        //It frequency equals speed of launch
+                        //And enable SpringJoint2D component
                         joint2D.distance = distanceVector.magnitude;
                         joint2D.frequency = launchSpeed;
                         joint2D.enabled = true;
                         break;
-
+                        
+                    //In case system is type Transform
                     case LaunchType.Transform:
-                        rb.gravityScale = 0;
-                        rb.velocity = Vector2.zero;
+                        rb.gravityScale = 0;        //Player's gravity equals 0
+                        rb.velocity = Vector2.zero; //Player's velocity equals 0
                         break;
                 }
             }
@@ -250,8 +320,11 @@ namespace Heaven
 
         private void OnDrawGizmosSelected()
         {
+            //If there is a fire point and max distance
             if (firePoint != null && hasMaxDistance)
             {
+                //Draw a green wire sphere around fire point of 
+                //with a radius equaling max distance
                 Gizmos.color = Color.green;
                 Gizmos.DrawWireSphere(firePoint.position, maxDistance);
             }

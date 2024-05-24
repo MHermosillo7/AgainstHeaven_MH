8de1c74e4ee73c1b783a0120d162ef5d359940e8
MHermosillo <143014585+MHermosillo7@users.noreply.github.com>
2024-05-24 07:01:14 -0600

    Documented GroundDetect script
    Although it is documented, whether it is actually being used is debatable.

--- Assets/Scripts/Player/GroundDetect.cs
+++ Assets/Scripts/Player/GroundDetect.cs
@@ -4,33 +4,44 @@ namespace Heaven
 {
     public class GroundDetect : MonoBehaviour
     {
-        PlayerMovement player;
-        PlayerJump playerJump;
+        PlayerMovement player;    //PlayerMovement script reference
+        PlayerJump playerJump;    //PlayerJump script reference
         // Start is called before the first frame update
         void Start()
         {
+            //Find PlayerMovement and PlayerJump scripts
             player = FindObjectOfType<PlayerMovement>();
             playerJump = FindObjectOfType<PlayerJump>();
         }
 
         private void OnCollisionEnter2D(Collision2D other)
         {
+            //When colliding with Ground Game Object
             if (other.gameObject.CompareTag("Ground"))
             {
+                //Player is grounded
                 player.isGrounded = true;
+                //Reset amount of jumps left
                 playerJump.jumpsLeft = playerJump.storeJumpsLeft;
 
+                //If jump buffer time is greater than 0 and
+                //Player has more than 0 jumps left
                 if (playerJump.jumpBufferTime > 0 && playerJump.jumpsLeft > 0)
                 {
+                    //Jump buffer time equals 0
                     playerJump.jumpBufferTime = 0;
+                    //Call PlayerJump's Jump method and pass
+                    //Integer 1 as its input
                     playerJump.Jump(1f);
                 }
             }
         }
         private void OnCollisionExit2D(Collision2D other)
         {
+            //When exiting collision with Ground Game Object
             if (other.gameObject.CompareTag("Ground"))
             {
+                //Player is no longer grounded
                 player.isGrounded = false;
             }
         }

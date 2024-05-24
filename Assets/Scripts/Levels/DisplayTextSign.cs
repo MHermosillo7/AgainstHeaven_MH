2564969e56014b83929b308df001f08d4bc6eb64
MHermosillo <143014585+MHermosillo7@users.noreply.github.com>
2024-05-23 22:20:10 -0600

    Documented DisplayTextSign script
    Only EndCutscene and Transition scripts are left as for the Level related scripts.

--- Assets/Scripts/Levels/DisplayTextSign.cs
+++ Assets/Scripts/Levels/DisplayTextSign.cs
@@ -6,35 +6,46 @@ namespace Heaven
     [RequireComponent(typeof(Collider2D))]
     public class DisplayTextSign : MonoBehaviour
     {
-        Collider2D collider;
-        [SerializeField] Text text;
-        [SerializeField] string displayText;
-        [SerializeField] GameObject panel;
+        Collider2D collider;                    //Collider reference
+        [SerializeField] Text text;             //Text reference
+        [SerializeField] string displayText;    //String to set displayed text
+        [SerializeField] GameObject panel;      //Panel game object
         // Start is called before the first frame update
         void Awake()
         {
+            //Get Collider component and make it a trigger
             collider = GetComponent<Collider2D>();
             collider.isTrigger = true;
+
+            //If there is a text reference
             if (text)
             {
+                //Leave text displayed blank
                 text.text = "";
+                //Disable panel game object
                 panel.SetActive(false);
             }
         }
 
+        //When object is triggered by player
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.gameObject.CompareTag("Player"))
             {
+                //Enable panel
                 panel.SetActive(true);
+                //Set text to text to display
                 text.text = displayText;
             }
         }
+        //When player exits the collider
         private void OnTriggerExit2D(Collider2D other)
         {
             if (other.gameObject.CompareTag("Player"))
             {
+                //Leave text blank
                 text.text = "";
+                //Disable panel
                 panel.SetActive(false);
             }
         }

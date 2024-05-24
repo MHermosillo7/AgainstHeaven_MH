0394357aa4b92b570d2758317a4302fc5f3f4be1
MHermosillo <143014585+MHermosillo7@users.noreply.github.com>
2024-05-24 07:57:26 -0600

    Documented RespawnPlayer script

--- Assets/Scripts/Player/RespawnPlayer.cs
+++ Assets/Scripts/Player/RespawnPlayer.cs
@@ -1,24 +1,28 @@
 using UnityEngine;
 
+//ATTACH TO HAZARD TILEMAP
 namespace Heaven
 {
     [RequireComponent(typeof(Collider2D))]
     public class RespawnPlayer : MonoBehaviour
     {
-        //ATTACH TO CAMERA
-        PlayerMovement player;
-        Collider2D collider;
+        PlayerMovement player;    //PlayerMovement script reference
+        Collider2D collider;      //Collider reference
         // Start is called before the first frame update
         void Start()
         {
+            //FInd PlayerMovement script
             player = FindObjectOfType<PlayerMovement>();
+            //Get Collider component
             collider = GetComponent<Collider2D>();
         }
 
+        //On Collision with Player Game Object
         private void OnCollisionEnter2D(Collision2D other)
         {
             if (other.gameObject.CompareTag("Player"))
-            {
+            {    
+                //Call PlayerMovement's Respawn method
                 player.Respawn();
             }
         }

eab217ad31211d6bd816f488111d3b1534fa820a
MHermosillo <143014585+MHermosillo7@users.noreply.github.com>
2024-05-24 07:11:59 -0600

    Documented MovementControl script
    It is important to note that, although this was supposed to be an essential script to control the force interferences, it was found too complex and difficult to implement. 
    
    Thus, this script was discarded and left unfinished during development and replaced for the AwayWall method in PlayerMovement script.

--- Assets/Scripts/Player/MovementControl.cs
+++ Assets/Scripts/Player/MovementControl.cs
@@ -1,19 +1,24 @@
 using UnityEngine;
 
+//ABANDONED SCRIPT
+//UNFINISHED
+//NO LONGER IN USE
+//Found a better, simpler solution later in development
 namespace Heaven
 {
     public class MovementControl : MonoBehaviour
     {
-        PlayerMovement player;
-        PlayerJump playerJump;
+        PlayerMovement player;        //PlayerMovement script reference
+        PlayerJump playerJump;        //PlayerJump script reference
 
-        RaycastHit2D hitLeft;
-        RaycastHit2D hitRight;
-        RaycastHit2D diagonalRight;
-        RaycastHit2D diagonalLeft;
+        RaycastHit2D hitLeft;         //Store RaycastHit in left direction
+        RaycastHit2D hitRight;        //Store RaycastHit in right direction
+        RaycastHit2D diagonalRight;   //Store RaycastHit in diagonal right direction
+        RaycastHit2D diagonalLeft;    //Store RaycastHit in diagonal left direction
         // Start is called before the first frame update
         void Awake()
         {
+            //Get PlayerMovement and PlayerJump scripts
             player = GetComponent<PlayerMovement>();
             playerJump = GetComponent<PlayerJump>();
         }
@@ -21,34 +26,51 @@ namespace Heaven
         // Update is called once per frame
         void LateUpdate()
         {
+            //Check whether there is wall in x direction
+            //ie. Call Check Wall method
             CheckWall();
         }
         void CheckWall()
         {
+            //If player is touching a wall
             if (playerJump.touchWall)
             {
+                //Shoot two Raycasts, one in left direction and other in right, and store
+                //the position where they hit an object
                 hitLeft = Physics2D.Raycast(transform.position, Vector2.left.normalized, .5f);
                 hitRight = Physics2D.Raycast(transform.position, Vector2.right.normalized,.5f);
 
+                //If there was a game object hit to the right with tag Wall
                 if (hitRight && hitRight.transform.gameObject.tag == "Wall")
                 {
+                    //If Input corresponds with left direction
                     if (Input.GetAxisRaw("Horizontal") == -1)
                     {
+                        //Player is no longer touching a wall
                         playerJump.touchWall = false;
+                        //Player is not sliding down a wall
                         playerJump.slideWall = false;
                     }
+                    //Else player cannot move
                     else player.canMove = false;
                 }
+                //If there was a game object hit to left with tag wall
                 if (hitLeft && hitLeft.transform.gameObject.tag == "Wall")
                 {
+                    //If input corresponds with right direction
                     if (Input.GetAxisRaw("Horizontal") == 1)
                     {
+                        //Print Warning "Right"
                         Debug.LogWarning("Right");
+                        //Player is not touching a wall
                         playerJump.touchWall = false;
+                        //Player is not sliding down a wall
                         playerJump.slideWall = false;
                     }
+                    //Else player cannot move
                     else player.canMove = false;
                 }
+                //If player is not touching a wall, return
                 else return;
             }
         }
